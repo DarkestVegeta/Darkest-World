287045d066c24694e5001842a278e603d34d3c9f
DarkestVegeta <Darkest-Family@hotmail.com>
2026-09-05 23:46:51 +0200

    Integrate falling star into living world

--- lib/widgets/living_world_scene.dart
+++ lib/widgets/living_world_scene.dart
@@ -2,6 +2,8 @@ import 'dart:math' as math;
 
 import 'package:flutter/material.dart';
 
+import 'falling_star_easter_egg.dart';
+
 /// Lightweight, asset-free animated background for the Darkest-World home.
 class LivingWorldScene extends StatefulWidget {
   final Widget child;
@@ -36,6 +38,7 @@ class _LivingWorldSceneState extends State<LivingWorldScene>
           ),
         ),
         widget.child,
+        const FallingStarEasterEgg(),
       ],
     );
   }
