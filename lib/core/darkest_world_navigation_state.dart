adb92864858bab47002da76e37e5d37e9bb8d7f0
DarkestVegeta <Darkest-Family@hotmail.com>
2026-09-15 02:35:49 +0200

    feat: track content navigation entry context

--- lib/core/darkest_world_navigation_state.dart
+++ lib/core/darkest_world_navigation_state.dart
@@ -8,6 +8,8 @@ class DarkestWorldNavigationState {
   final ContentItem? next;
   final List<ContentItem> related;
   final String source;
+  final String entryPoint;
+  final String? originId;
 
   const DarkestWorldNavigationState({
     required this.previous,
@@ -15,6 +17,8 @@ class DarkestWorldNavigationState {
     required this.next,
     required this.related,
     required this.source,
+    this.entryPoint = 'direct',
+    this.originId,
   });
 
   String? get previousId => previous?.id;
@@ -22,4 +26,24 @@ class DarkestWorldNavigationState {
   String? get nextId => next?.id;
 
   List<String> get relatedIds => related.map((item) => item.id).toList(growable: false);
+
+  DarkestWorldNavigationState copyWith({
+    ContentItem? previous,
+    ContentItem? current,
+    ContentItem? next,
+    List<ContentItem>? related,
+    String? source,
+    String? entryPoint,
+    String? originId,
+  }) {
+    return DarkestWorldNavigationState(
+      previous: previous ?? this.previous,
+      current: current ?? this.current,
+      next: next ?? this.next,
+      related: related ?? this.related,
+      source: source ?? this.source,
+      entryPoint: entryPoint ?? this.entryPoint,
+      originId: originId ?? this.originId,
+    );
+  }
 }
