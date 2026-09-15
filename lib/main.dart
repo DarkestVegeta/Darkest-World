456c44565bdcc9b56daff17ba584e14f4d248c33
DarkestVegeta <Darkest-Family@hotmail.com>
2026-09-15 04:33:21 +0200

    GO206: mount cinematic archive depth layer

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'widgets/darkest_world_archive_atmosphere.dart';
 import 'widgets/darkest_world_cinematic_optics.dart';
 import 'widgets/darkest_world_archive_lens.dart';
 import 'widgets/darkest_world_archive_stage.dart';
+import 'widgets/darkest_world_archive_depth.dart';
 
 final GlobalKey<NavigatorState> darkestWorldNavigatorKey = GlobalKey<NavigatorState>();
 final DarkestWorldNavigationObserver darkestWorldNavigationObserver = DarkestWorldNavigationObserver();
@@ -40,6 +41,7 @@ class DarkestWorldApp extends StatelessWidget {
           const DarkestWorldCinematicOptics(),
           const DarkestWorldArchiveLens(),
           const DarkestWorldArchiveStage(),
+          const DarkestWorldArchiveDepth(),
           const DarkestWorldContentNavigation(),
           DarkestWorldRouteHistory(
             navigatorKey: darkestWorldNavigatorKey,
