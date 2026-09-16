9e69b58d4df15c56ab237e3dcb1271b92a4e7252
DarkestVegeta <Darkest-Family@hotmail.com>
2026-09-16 03:49:48 +0200

    GO269: add single archive boundary regression

---
+++ test/darkest_world_archive_signal_single_boundary_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:darkest_world/core/content_models.dart';
+import 'package:darkest_world/core/darkest_world_navigation_state.dart';
+import 'package:darkest_world/widgets/archive_signal_telemetry_lens.dart';
+
+ContentItem _item(String id) => ContentItem.fromRow({
+  'id': id,
+  'content_type': 'game',
+  'title': id,
+  'slug': id,
+});
+
+void main() {
+  testWidgets('single archive item exposes only the current rail state', (tester) async {
+    final state = DarkestWorldNavigationState(
+      previous: null,
+      current: _item('current'),
+      next: null,
+      related: const [],
+      source: 'detail',
+    );
+
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(body: ArchiveSignalTelemetryLens(state: state)),
+    ));
+
+    expect(find.text('CURRENT'), findsOneWidget);
+    expect(find.text('NO PREVIOUS'), findsOneWidget);
+    expect(find.text('NO NEXT'), findsOneWidget);
+  });
+}
